c0e19d78b473d78eadceb52df4a1d716b1bb46db
tikalestari <tika.dwilestari@gmail.com>
2015-11-06 20:00:00 -0600

    Update

---
+++ ACM/pinspired/Colors.py
@@ -0,0 +1,18 @@
+def function():
+    t = int(input())
+    for i in range(t):
+        n, c = input().split()
+        n = int(n)
+        order_list = []
+        query = c[1:]
+        r_q = int(query[0:2], 16)
+        g_q = int(query[2:4], 16)
+        b_q = int(query[4:6], 16)
+        euc = 
+        for j in range(n):
+            color = input()
+
+
+
+if __name__ == "__main__":
+    function()
