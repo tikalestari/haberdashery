c0d5c127a35093988a4c9f050f5255e59d240d0c
tikalestari <tika.dwilestari@gmail.com>
2016-10-26 01:04:43 -0500

    deleted comments

--- Implementation - Day 2/Moving Average/stocks.py
+++ Implementation - Day 2/Moving Average/stocks.py
@@ -1,6 +1,6 @@
 def stocks():
     t = int(input())
-    for i in range(0,t): #iterate through input
+    for i in range(0,t): 
         print("Test " + str(i+1) + ":")
         l = list(map(int, input().split()))
         s = 0
@@ -9,17 +9,6 @@ def stocks():
             s += i
             print("Day " + str(d) + ":", str(s//d))
             d += 1
-
-        #for i in l:
-        #    d = 1
-        #    print("Day " + str(d) + ": ", end="")
-        #    s = 0
-        #    for j in range(0, d):
-        #        s += l[j]
-        #    print(s // i)
-        #    d += 1
-
-
 
 if __name__ == "__main__":
     stocks()
