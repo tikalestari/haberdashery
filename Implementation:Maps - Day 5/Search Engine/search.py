9d82f0a02910832e59ae93e12f31b84089b0c234
tikalestari <tika.dwilestari@gmail.com>
2016-10-28 15:00:48 -0500

    done

--- Implementation:Maps - Day 5/Search Engine/search.py
+++ Implementation:Maps - Day 5/Search Engine/search.py
@@ -2,26 +2,24 @@ def search():
     t = int(input())
     for i in range(0,t):
         n,m = map(int,input().split())
-        letter_to_count_to_web = {}
-        count_to_web = {}
-        web_to_count = {}
+        letter_map = {}
         for i in range(0,n):
             site = input()
             info_line = input().split()
             for i in info_line:
-                if i not in letter_to_count_to_web:
-                    letter_to_count_to_web[i] = {}
-                if site not in letter_to_count_to_web[i]:
-                    letter_to_count_to_web[i][site] = 1
+                if i not in letter_map:
+                    letter_map[i] = {}
+                if site not in letter_map[i]:
+                    letter_map[i][site] = 1
                 else:
-                    letter_to_count_to_web[i][site] += 1
+                    letter_map[i][site] += 1
 
         for i in range(0,m):
             q = input()
-            if q not in letter_to_count_to_web:
+            if q not in letter_map:
                 print("NONE")
             else:
-                sort = sorted(letter_to_count_to_web[q], key=letter_to_count_to_web[q].get, reverse=True) #sort by value
+                sort = sorted(letter_map[q], key=letter_map[q].get, reverse=True) #sort by value
                 for i in range(0,len(sort)):
                     print(str(i+1) + ". " + sort[i])
 
