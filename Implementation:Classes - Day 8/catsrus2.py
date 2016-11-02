b32af87b0b5ca91b92fe0aae72f1d836a6b1c090
tikalestari <tika.dwilestari@gmail.com>
2016-11-01 23:38:30 -0500

    progress (redo of catsrus.py)

--- Implementation:Classes - Day 8/catsrus2.py
+++ Implementation:Classes - Day 8/catsrus2.py
@@ -1,5 +1,10 @@
-class Store():
-    def __init__(self, bn, b, p): 
+class Cat():
+    def __init__(self, br, bn, b, p, i):
+        bulk_to_cost = {}
+        breed_price = {}
+        breed_bulk = {}
+        profit = 0
+        total_cost = 0
         self.bn = bn
         self.b = b
         self.p = p
@@ -13,6 +18,19 @@ class Store():
     def price(self):
         return self.p
 
+    def inventory(self):
+        return self.i
+
+    def transact(self, n):
+        if self.inventory() < n:
+            while self.inventory() < n:
+                self.i += self.bulk()
+                total_cost += self.bulk_cost()
+                print("BUY: " + b)
+        if self.inventory() >= n:
+            profit += self.price() * n
+            self.i -=
+
 def catsrus():
     cats = {}
     profit = 0
@@ -20,17 +38,18 @@ def catsrus():
     n,m = map(int,input().split())
     for i in range(n):
         breed, bulk, bulk_price, price = input().split()
-        cats[breed] = [Store(bulk, bulk_price, price),0]
+        cats[breed] = Cat(breed, bulk, bulk_price, price, 0)
     for i in range(m):
         b, n = input().split()
-        if cats[b][1] < int(n):
+        '''if cats[b][1] < int(n):
             while cats[b][1] < int(n):
                 cats[b][1] += int(cats[b][0].bulk())
                 total_cost += int(cats[b][0].bulk_cost())
                 print("BUY: " + b)
         if cats[b][1] >= int(n):
             profit += int(cats[b][0].price()) * int(n)
-            cats[b][1] -= int(n)
+            cats[b][1] -= int(n)'''
+        cats[b].transact(n)
     print("PROFIT: " + str(profit - total_cost))
 
 if __name__ == "__main__":
