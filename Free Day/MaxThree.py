48479d9af7a48a23b32bd559c6e0098f1cb3973c
tikalestari <tika.dwilestari@gmail.com>
2016-11-11 00:27:53 -0600

    Finished Problem

--- Free Day/MaxThree.py
+++ Free Day/MaxThree.py
@@ -1,1 +1,26 @@
-'Print the max of 3 items in a list in one pass'
+'Print the max 3 items in a list in one pass'
+def max_three(l):
+    first = l[0]
+    second = l[0]
+    third = l[0]
+    for i in range(len(l)):
+        if l[i] > first:
+            if second < first:
+                if third < second:
+                    third = second
+                second = first
+            first = l[i]
+        else:
+            if l[i] > second:
+                third = second
+                second = l[i]
+            else:
+                if l[i] > third:
+                    third = l[i]
+
+    return str(first) + " " + str(second) + " " + str(third)
+
+
+l = list(map(int, input().strip().split(' ')))
+answer = max_three(l);
+print(answer)
