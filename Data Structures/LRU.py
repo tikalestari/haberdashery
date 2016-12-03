1ce0a4e40ce56c08b2a05dd9311972530e3c7361
tikalestari <tika.dwilestari@gmail.com>
2016-12-02 21:26:08 -0600

    Started

---
+++ Data Structures/LRU.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+
+class LruCache:
+    def __init__(self):
+
+    def find(self, value):
+        
