0e3a6d957b20a424bf104a67809e4877bc78b3d8
tikalestari <tika.dwilestari@gmail.com>
2016-12-13 23:21:47 -0600

    Update

--- Recursion/Problem_10.py
+++ Recursion/Problem_10.py
@@ -1,5 +1,6 @@
 '''
-Given a matrix, find the max path from top left corner to bottom right corner
+Given N, the length of the row and column of a NxN matrix,
+find the number of paths from top left corner to bottom right corner
 only going down and right
 
 NxN matrix is
@@ -12,5 +13,13 @@ l = [[0 for i in range(n)] for i in range (n)]
 
 '''
 
-def path(matrix):
-    if matrix[]
+def path(n, row, col):
+    if row > n-1 or col > n-1:
+        return 0
+    if row == n-1 and col == n-1:
+        return 1
+    return path(n, row + 1, col) + path(n, row, col + 1)
+
+
+l = [[0 for i in range(3)] for i in range (3)]
+print(path(3,0,0))
