ee3d9fc5496120bd5b4822dfd1c19bb40eda03e0
tikalestari <tika.dwilestari@gmail.com>
2016-12-16 16:14:51 -0600

    Update

--- Recursion/Problem_15.py
+++ Recursion/Problem_15.py
@@ -1,22 +1,41 @@
 '''
-Given a grid of 1's and 0's, return the number of blobs
+Given a grid of 1's and 0's, return the number of islands
 made up of 1's.
 
 Example:
 [1,0,0,1,1]
 [1,1,1,0,1]
-[1,1,0,1,1] = 1 blob
+[1,1,0,1,1] = 1 island
 
 [1,0,0,0,1]
 [1,1,1,0,1]
-[1,1,0,0,1] = 2 blobs
+[1,1,0,0,1] = 2 islands
 
 '''
 
-def num_blobs(matrix):
+def num_islands(matrix):
+    count = 0
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
-            if matrix[row][col] == 0:
-                zero_out_blob(matrix, row, col)
+            if matrix[row][col] == 1:
+                count += 1
+                zero_out_island(matrix, row, col)
+    return count
 
-def zero_out_blob(matrix, row, col):
+def zero_out_island(matrix, row, col):
+    if row >= len(matrix) or col >= len(matrix[0]):
+        return
+    elif row < 0 or col < 0:
+        return
+    elif matrix[row][col] == 0:
+        return
+    matrix[row][col] = 0
+    zero_out_island(matrix, row+1, col)
+    zero_out_island(matrix, row, col+1)
+    zero_out_island(matrix, row-1, col)
+    zero_out_island(matrix, row, col-1)
+
+
+
+m = [[1,0,0,0,1],[1,1,1,0,1],[1,1,0,0,1]]
+print(num_islands(m))
