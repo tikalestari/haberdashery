d2959171ceb8de8e784e16f61540d34c38b52a4a
tikalestari <tika.dwilestari@gmail.com>
2016-12-20 22:59:49 -0600

    Added new files

---
+++ CTCI Problems/CH.1 - Arrays and Strings/1.1.py
@@ -0,0 +1,20 @@
+'''Implement an algorithm to determine if a string has all
+unique characters. What if you can't use additional
+data structures?'''
+
+def unique_1(s):
+    characters = {} # make the keys the letters of the alphabet
+    if len(s) > 26
+        return false
+    for i in len(s)-1:
+        key = s[i:i+1]
+        if key in characters:
+
+
+        else:
+            characters[key] = true
+                return false
+
+if __name__ == '__main__':
+    while True:
+        print(unique_1(input()))
