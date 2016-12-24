c8456cb313f82f643421b127a408a9b481807865
tikalestari <tika.dwilestari@gmail.com>
2016-12-23 21:55:58 -0600

    Update

--- Recursion/Problem_25.py
+++ Recursion/Problem_25.py
@@ -5,4 +5,21 @@ and you return T/F if it can be written
 using only words in the dictionary
 '''
 
-def function()
+def function(words,s,start,end,result):
+    if s[start:end] in words:
+        result += s[start:end]
+        start = end
+    if end == len(s):
+        if result == s:
+            print("True")
+            return True
+        else:
+            print("False")
+            return False
+    function(words,s,start,end+1,result)
+
+
+
+words = ["bar", "cat", "foo"]
+s = "foobar"
+function(words,s,0,0,"")
