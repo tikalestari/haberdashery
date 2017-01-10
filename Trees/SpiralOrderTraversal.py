6275cbe87fd74f856424055daddd70d2f485279d
tikalestari <tika.dwilestari@gmail.com>
2017-01-09 23:54:23 -0600

    Update

--- Trees/SpiralOrderTraversal.py
+++ Trees/SpiralOrderTraversal.py
@@ -1,12 +1,12 @@
 '''
 Print the spiral order traversal of a tree using BFS
-    1
-  /    \
+     1
+   /    \
   3      5
  / \    /  \
 7   9  11  13
 
-spiral(1) = 1 5 3 7 9 11 13
+spiral(1) = 1 3 5 13 11 9 7
 '''
 from collections import deque
 
@@ -16,8 +16,22 @@ class Node:
         self.children = children
 
 def spiral(root):
-    s = 
+    s = []
     q = deque()
+    s.append(root)
+    while len(q) > 0 or len(s) > 0:
+        size_s = len(s)
+        for i in range(size_s):
+            node = s.pop()
+            print(str(node.value)+" ",end="")
+            for n in node.children:
+                q.append(n)
+        size_q = len(q)
+        for j in range(size_q):
+            node = q.popleft()
+            print(str(node.value)+" ",end="")
+            for n in node.children:
+                s.append(n)
 
 
 g = Node(13,[])
@@ -27,4 +41,4 @@ d = Node(7,[])
 c = Node(5,[f,g])
 b = Node(3,[d,e])
 a = Node(1,[b,c])
-level_order(a)
+spiral(a)
