4a96957cc6b0b16d5b1e18350d478d79f9b4f2f2
tikalestari <tika.dwilestari@gmail.com>
2017-01-12 12:22:22 -0600

    Update:

--- Trees/ShortestPathOfficeBFS.py
+++ Trees/ShortestPathOfficeBFS.py
@@ -14,7 +14,7 @@ def main(matrix,k):
         for col in range(len(matrix[0])):
             if matrix[row][col] == 0:
                 steps = bfs(matrix,(row,col),k)
-                if steps < 0:
+                if not steps:
                     return False
                 matrix[row][col] = steps
     for row in matrix:
@@ -25,7 +25,7 @@ def bfs(matrix,cell,k):
     q = deque()
     q.append(cell)
     steps = 0
-    directions = [-1,0,1]
+    directions = [[1,0],[0,1],[-1,0],[0,-1]]
     while len(q) > 0:
         steps += 1
         size = len(q)
@@ -33,25 +33,21 @@ def bfs(matrix,cell,k):
             current = q.popleft()
             row = current[0]
             col = current[1]
-            for r in directions:
-                for c in directions:
-                    if r != c and r - c != 0 and c + r != 0:
-                        print("current: "+ str(r) + " "+ str(c))
-                        print("row: "+ str(row+r))
-                        print("col: "+str(col+c))
-                        print("steps: "+str(steps))
-                        if row+r >= len(matrix) or col+c >= len(matrix[0]):
-                            pass
-                        elif row+r < 0 or col+c < 0:
-                            pass
-                        elif str(matrix[row+r][col+c]) == 'x':
-                            pass
-                        elif str(matrix[row+r][col+c]) == 'm' and steps <= k:
-                            return steps
-                        elif int(matrix[row+r][col+c]) >= 0:
-                            q.append((row+r,col+c))
-                        print(q)
-                        print()
+            for d in directions:
+                r = d[0]
+                c = d[1]
+                if row+r >= len(matrix) or col+c >= len(matrix[0]):
+                    pass
+                elif row+r < 0 or col+c < 0:
+                    pass
+                elif str(matrix[row+r][col+c]) == 'x':
+                    pass
+                elif str(matrix[row+r][col+c]) == 'm' and steps > k:
+                    return -1
+                elif str(matrix[row+r][col+c]) == 'm' and steps <= k:
+                    return steps
+                else:
+                    q.append((row+r,col+c))
     return -1
 
 
