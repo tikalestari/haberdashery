e9a3934e8aece21c7bdd7d4e11cc94241928ccec
tikalestari <tika.dwilestari@gmail.com>
2017-01-20 17:36:43 -0600

    Update:

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -8,32 +8,70 @@ class Node:
         self.left = None
         self.right = None
 
-def bst_add(root, node):
-    if root is None:
-        root = node
-    elif root.value > node.value:
-        if root.left is None:
-            root.left = node
-        else:
-            bst_add(root.left, node)
-    elif root.right is None:
-        root.right = node
-    else:
-        bst_add(root.right, node)
+class BST:
+    def __init__(self):
+        self.root = None
 
-def print_tree(root):
-    if not root:
-        return
-    print_tree(root.left)
-    print(root.value)
-    print_tree(root.right)
+    def add(self, node):
+        r = self.root
+        self.add_helper(r, node)
+
+    def add_helper(self, current, node):
+        if current is None:
+            current = node
+        elif node.value > current.value:
+            if current.right is None:
+                current.right = node
+            else:
+                self.add_helper(current.right, node)
+        else:
+            if current.left is None:
+                current.left = node
+            else:
+                self.add_helper(current.left, node)
+
+    def find(self, value):
+        r = self.root
+        self.find_helper(r, value)
+
+    def find_helper(self, current, value):
+        if current.value == value:
+            print(str(value) + " is in this tree.")
+        elif value > current.value:
+            if current.right is None:
+                print(str(value) + " is NOT in this tree.")
+            else:
+                self.find_helper(current.right, value)
+        else:
+            if current.left is None:
+                print(str(value) + " is NOT in this tree.")
+            else:
+                self.find_helper(current.left, value)
+
+    def remove():
+        pass
+
+    def print_tree(self):
+        r = self.root
+        self.print_tree_helper(r)
+
+    def print_tree_helper(self, node):
+        if not node:
+            return
+        self.print_tree_helper(node.left)
+        print(node.value)
+        self.print_tree_helper(node.right)
 
 a = Node(5)
 b = Node(4)
 c = Node(3)
 d = Node(7)
-bst_add(a,b)
-bst_add(a,c)
-bst_add(a,d)
 
-print_tree(a)
+tree = BST()
+tree.root = a
+tree.add(b)
+tree.add(c)
+tree.add(d)
+
+tree.print_tree()
+tree.find(5)
