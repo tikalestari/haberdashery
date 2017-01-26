65a46b7b273b79cf633d34e11577daffe3ee50e7
tikalestari <tika.dwilestari@gmail.com>
2017-01-25 23:09:59 -0600

    Update

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -7,6 +7,7 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+        self.parent = None
 
 class BST:
     def __init__(self):
@@ -22,11 +23,13 @@ class BST:
         elif node.value > current.value:
             if current.right is None:
                 current.right = node
+                node.parent = current
             else:
                 self.add_helper(current.right, node)
         else:
             if current.left is None:
                 current.left = node
+                node.parent = current
             else:
                 self.add_helper(current.left, node)
 
@@ -67,38 +70,59 @@ class BST:
 
     def find_min(self):
         r = self.root
-        self.find_min_helper(r)
+        return self.find_min_helper(r)
 
     def find_min_helper(self, root):
         if root.left is None:
             print("The minimum of this tree is "+ str(root.value))
             return root
-        self.find_min_helper(root.left)
+        return self.find_min_helper(root.left)
 
     def find_max(self):
         r = self.root
-        self.find_max_helper(r)
+        return self.find_max_helper(r)
 
     def find_max_helper(self, root):
         if root.right is None:
             print("The maximum of this tree is "+ str(root.value))
             return root
-        self.find_max_helper(root.right)
+        return self.find_max_helper(root.right)
+
 
     def find_successor(self, value):
         r = self.root
-        if value == self.find_max(r).value:
+        m = self.find_max()
+        if value == m.value:
             print("No successor")
             return None
-        self.find_successor_helper(r, value)
+        return self.find_successor_helper(r,value)
+
+# if value is max, no successor
+# if value has a right subtree, successor is minimum of that subtree,
+# if doesnt have subtree and is not max, the successor is one of its ancestors
 
     def find_successor_helper(self, current, value):
         if current.value == value:
-            if current.
             if current.left is None and current.right is None:
-
+                p = current.parent
+                while p.value < value:
+                    p = p.parent
+                result = p
+                print("Successor of "+str(value)+" is "+ str(result.value))
             if current.right:
-                return self.find_min(current.right)
+                result = self.find_min_helper(current.right)
+                print("Successor of "+str(value)+" is "+ str(result.value))
+                return result
+        elif value > current.value:
+            if current.right is None:
+                print(str(value) + " is NOT in this tree.")
+            else:
+                self.find_successor_helper(current.right, value)
+        else:
+            if current.left is None:
+                print(str(value) + " is NOT in this tree.")
+            else:
+                self.find_successor_helper(current.left, value)
 
     def print_tree(self):
         r = self.root
@@ -111,18 +135,29 @@ class BST:
         print(node.value)
         self.print_tree_helper(node.right)
 
-a = Node(5)
-b = Node(4)
-c = Node(3)
-d = Node(7)
+a = Node(8)
+b = Node(3)
+c = Node(1)
+d = Node(6)
+e  = Node(4)
+f  = Node(7)
+g = Node(10)
+h  = Node(14)
+i  = Node(13)
 
 tree = BST()
 tree.root = a
 tree.add(b)
 tree.add(c)
 tree.add(d)
+tree.add(e )
+tree.add(f )
+tree.add(g)
+tree.add(h )
+tree.add(i )
 
 tree.print_tree()
 tree.find(5)
 tree.find_min()
 tree.find_max()
+tree.find_successor(10)
