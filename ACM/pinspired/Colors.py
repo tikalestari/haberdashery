ed413588b018da644c69be4df2bcef8ac027ff4b
tikalestari <tika.dwilestari@gmail.com>
2017-02-15 20:17:10 -0600

    Update

--- ACM/pinspired/Colors.py
+++ ACM/pinspired/Colors.py
@@ -11,8 +11,15 @@ def function():
         b_q = int(query[4:6], 16)
         for j in range(n):
             color = input()
-
-
+            r_c = int(color[1:3], 16)
+            g_c = int(color[3:5], 16)
+            b_c = int(color[5:7], 16)
+            euc = math.sqrt(math.pow(r_q - r_c,2)+math.pow(g_q - g_c,2)+math.pow(b_q - b_c,2))
+            order_list.append((euc,color))
+        sorted(order_list, key=lambda tup: tup[0])
+        print("Case "+ str(c)+":")
+        for n in order_list:
+            print(n[1])
 
 if __name__ == "__main__":
     function()
