114995aa430573145ec01c7fda477546fc04b8f5
tikalestari <tika.dwilestari@gmail.com>
2017-02-20 00:43:20 -0600

    Update

--- Practice Problems/WordSearch.py
+++ Practice Problems/WordSearch.py
@@ -20,33 +20,33 @@ word = "ABCB", -> returns false.
 
 def search(board, word):
     letters = list(word)
+    visited = [[1 for c in range(len(board[0]))] for j in range(len(board))]
     for r in range(len(board)):
         for c in range(len(board[0])):
             if board[r][c] == letters[0]:
-                if check(board, r, c, word, 1):
+                if check(board, visited, r, c, word, 1):
                     return True
-
     return False
 
-def check(board, row, col, word, index):
+def check(board, visited, row, col, word, index):
     if row >= len(board) or col >= len(board[0]):
         return False
     if row < 0 or col < 0:
         return False
-    if board[row][col] == 0:
+    if visited[row][col] == 0:
         return False
     if index >= len(word):
         return False
 
     if board[row][col] == word[index]:
-        board[row][col] = 0
+        visited[row][col] = 0
         return True
 
     else:
-        return check(board, row, col+1, word, index+1) or \
-        check(board, row, col-1, word, index+1) or \
-        check(board, row+1, col, word, index+1) or \
-        check(board, row-1, col, word, index+1)
+        return check(board, visited, row, col+1, word, index+1) or \
+        check(board, visited, row, col-1, word, index+1) or \
+        check(board, visited, row+1, col, word, index+1) or \
+        check(board, visited, row-1, col, word, index+1)
 
     return False
 
@@ -56,3 +56,6 @@ b = [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']]
 print(search(b,"ABCCED"))
 print(search(b,"SEE"))
 print(search(b,"ABCB"))
+print(search(b,"ABCE"))
+print(search(b,"SFCS"))
+print(search(b,"ADEE"))
